ec47d9503e2acfd61363e6d6edd18c68874dfd91
lo3xman-arch <lo3xman@gmail.com>
2026-08-24 22:04:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,10 @@
+const APP_CONFIG = {
+    name: "SIMPEG",
+    title: "Sistem Informasi Kepegawaian",
+    description: "UPTD PUSKESMAS BOJONGSARI",
 
+    // CUKUP GANTI URL INI jika nanti menggunakan aplikasi lain
+    appUrl: "https://script.google.com/a/~/macros/s/AKfycbxpDp2Puft0Ycxo08EstUsugllx9taatv-iwm1mIW95Uy62W-OOlxrq8pdjtiUwLep_/exec",
+
+    year: "2026"
+};
